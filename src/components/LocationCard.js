52653a2287a23d7716af5b453ba4234db61c2cb8
Chris York <christopherayork1992@gmail.com>
2019-07-26 11:48:49 -0400

    Locations done

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import styled from 'styled-components';
 
-export default function LocationCard ({ name, type, dimension, residents }) {
-  // image={image}
-  return (<span>todo: location</span>
-  )
+const LocationDiv = styled.div`
+  width: 25%;
+`;
+
+export default function LocationCard (props) {
+  let location = props.location;
+  return (
+      <LocationDiv>
+        <h3>{location.name}</h3>
+        <ul>
+          <li>Type: {location.type}</li>
+          <li>Dimension: {location.dimension}</li>
+          {/* Not producing tons of extra get requests to list out resident names!! */}
+        </ul>
+      </LocationDiv>
+  );
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import LocationCard from './LocationCard';
 
 export default function LocationsList() {
+  let [content, setContent] = useState(null);
 
+  useEffect(() => {
+
+    axios.get('https://rickandmortyapi.com/api/location/')
+        .then(res => {
+          setContent(res.data);
+          console.log(res);
+        });
+  }, []); // no dependencies needed
+
+  if(!content) return <div>Loading Teleporters...</div>
+
+  return(
+      <section className='location-list grid-view'>
+        {content.results.map(l => <LocationCard location={l} />)}
+      </section>
+  );
 }
